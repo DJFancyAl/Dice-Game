d45a3586e4eabdeeb6c08630860bfe50b9243017
DJFancyAl <abluemleiv@gmail.com>
2022-11-21 19:32:10 -0500

    Coin Flipper Works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,55 @@
 // TODO: Declare any global variables we need
+const content = document.getElementById('buttons');
+const coinImage = document.getElementById('coin-image');
+buttons.style.border = '1px solid black';
+buttons.style.padding = '15px';
+const flipPenny = document.createElement("button");
+flipPenny.style.border = '1px solid black'
+flipPenny.style.backgroundColor = 'lightblue'
+flipPenny.textContent = 'Flip The Penny!'
+buttons.append(flipPenny);
+const clearScoreboard = document.createElement("button");
+clearScoreboard.style.border = '1px solid black'
+clearScoreboard.style.backgroundColor = 'orange'
+clearScoreboard.textContent = 'Clear Scoreboard'
+buttons.append(clearScoreboard);
+let heads = document.getElementById('heads')
+const headsPercentDisplay = document.getElementById('heads-percent')
+let tailsPercentDisplay = document.getElementById('tails-percent')
+let tails = document.getElementById('tails')
+let headsValue = 0
+let tailsValue = 0
+let totalFlips = 0
+let headsPercent = 0
+let tailsPercent = 0
 
+flipPenny.addEventListener('click', () => {
+    let randomValue = Math.random();
+    if(Math.round(randomValue) === 1){
+        coinImage.src = "./assets/images/penny-heads.jpg"
+        headsValue ++
+        heads.innerText = headsValue;
+    } else {
+        coinImage.src = "./assets/images/penny-tails.jpg"
+        tailsValue ++
+        tails.innerText = tailsValue;
+    }
+    totalFlips ++
+    headsPercent = (headsValue/totalFlips)*100;
+    headsPercentDisplay.innerText = Math.round(headsPercent) + "%";
+    tailsPercent = (tailsValue/totalFlips)*100;
+    tailsPercentDisplay.innerText = Math.round(tailsPercent) + "%";
 
-document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
+})
 
-    // TODO: Add event listener and handler for flip and clear buttons
-
-    // Flip Button Click Handler
-        // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
-
-        // Update the scorboard
-        // TODO: Calculate the total number of rolls/flips
-        // Make variables to track the percentages of heads and tails
-        // TODO: Use the calculated total to calculate the percentages
-        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-        // TODO: Update the display of each table cell
-
-
-    // Clear Button Click Handler
-        // TODO: Reset global variables to 0
-        // TODO: Update the scoreboard (same logic as in flip button click handler)
-
+clearScoreboard.addEventListener('click', () => {
+    headsValue = 0;
+    tailsValue = 0;
+    totalFlips = 0;
+    headsPercent = 0;
+    tailsPercent = 0;
+    heads.innerText = 0;
+    tails.innerText = 0;
+    headsPercentDisplay.innerText = '0%';
+    tailsPercentDisplay.innerText = '0%';
 })
